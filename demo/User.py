258dbfc523119013b85c3dcca327bda0bb220940
ada <735529126@qq.com>
2020-12-27 21:29:47 +0800

    11

--- demo/User.py
+++ demo/User.py
@@ -4,10 +4,17 @@ class User:
   def show(self):
     print(self.name)
 
+  @classmethod
+  def class_foo(cls):
+      print(cls)
+
+  def printMessage(self):
+    print("hi")
 
 user =User("ada66")
 user.show()
 user.age=35
+user.class_foo()
 print(user.age)
 def showMsg():
   print("hi")
